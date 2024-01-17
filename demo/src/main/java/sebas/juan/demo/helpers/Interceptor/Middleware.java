8f1fef9399a933f22a0878c2f84d487d2b2e8920
Juan Romero <80675013+Juanies@users.noreply.github.com>
2024-01-17 15:00:16 +0100

    Add files via upload

--- demo/src/main/java/sebas/juan/demo/helpers/Interceptor/Middleware.java
+++ demo/src/main/java/sebas/juan/demo/helpers/Interceptor/Middleware.java
@@ -3,7 +3,7 @@ package sebas.juan.demo.helpers.Interceptor;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-
+import java.util.Arrays;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,23 +14,20 @@ public class Middleware implements HandlerInterceptor {
     @Override
     public boolean preHandle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) throws Exception {
         Cookie[] cookies = request.getCookies();
+        System.out.println("Cookies: " + Arrays.toString(cookies));
 
         if (cookies != null) {
             for (Cookie cookie : cookies) {
-                if ("cookie".equals(cookie.getName())) {
-                    String token = cookie.getValue();
-                    if (Token.validateToken(token)) {
-                        String username = Token.getName(token);
-
-                        // Almacena el nombre de usuario en el atributo de la solicitud
-                        request.setAttribute("username", username);
-
-                        return true; // Continúa con la ejecución del controlador
-                    }
+                System.out.println("Cookie: " + cookie.getName() + "=" + cookie.getValue());
+                if ("nombreDeTuCookie".equals(cookie.getName())) {
+                    // Resto del código...
                 }
             }
+        } else {
+            System.out.println("No se encontraron cookies en la solicitud.");
         }
 
+        System.out.println(cookies);
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Usuario no autenticado");
         return false;
     }
